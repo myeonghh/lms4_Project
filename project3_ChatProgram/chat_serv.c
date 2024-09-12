ec98741798e87ab490df92a0987a2e19b1a2988e
myeonghh <poiuson123@naver.com>
2024-09-12 12:30:35 +0900

    프로젝트 수정

--- project3_ChatProgram/chat_serv.c
+++ project3_ChatProgram/chat_serv.c
@@ -34,12 +34,14 @@ typedef struct user
 	char f_answer[100];
 	int socket_num;
 	int state; // 1: 로그인, 2: 로그아웃, 3: 회원탈퇴, 
+	int room_num; // 현재 유저의 채팅방 번호 => 로비 채팅방 1번
 	LETTER letter_box[10];
 
 }USER;
 
-USER reg_user_list[MAX_REG_USER] = {0,};
-
+USER reg_user_list[MAX_REG_USER] = 
+{{1, "qwer", "1111", "cookie", "01055555555", "ppp@naver.com", "안녕", "하세요", 0, 2, 0},
+ {2, "ssss1234", "1234", "뚜비", "01044444444", "aoui@naver.com", "반갑", "습니다", 0, 2, 0}};
 
 
 
@@ -47,7 +49,7 @@ void * handle_clnt(void * arg);
 void send_msg(char * msg, int len);
 void error_handling(char * msg);
 
-int reg_user_cnt = 0;
+int reg_user_cnt = 2;
 int clnt_cnt = 0;
 int clnt_socks[MAX_CLNT];
 pthread_mutex_t mutx;
@@ -95,6 +97,7 @@ int main(int argc, char *argv[])
 	
 void * handle_clnt(void * arg)
 {
+	USER p_user = {0,};
 	// int user_num;
 	// char id[MAX_ID_LEN];
 	// char pw[MAX_PW_LEN];
@@ -104,7 +107,8 @@ void * handle_clnt(void * arg)
 	// char f_question[100];
 	// char f_answer[20];
 	int clnt_sock=*((int*)arg);
-	int str_len=0, i, choice_num, dup_chk, login_chk, back_chk;
+	int str_len = 0; 
+	int i, choice_num, dup_chk, login_chk, back_chk, pnum_chk;
 	char msg[BUF_SIZE];
 	char choice_str[BUF_SIZE];
 	char id_str[BUF_SIZE];
@@ -137,6 +141,7 @@ void * handle_clnt(void * arg)
 				back_chk = 0;
 
 				write(clnt_sock, "[ 아이디를 입력하세요 (메뉴로 돌아 가려면 'b' 입력) ]\n", strlen("[ 아이디를 입력하세요 (메뉴로 돌아 가려면 'b' 입력) ]\n"));
+				memset(id_str, 0, sizeof(id_str));
 				str_len = read(clnt_sock, id_str, sizeof(id_str));
 				printf("입력한 아이디 %s", id_str);
 				id_str[str_len-1] = '\0';
@@ -148,6 +153,7 @@ void * handle_clnt(void * arg)
 				}
 
 				write(clnt_sock, "[ 비밀번호를 입력하세요 (메뉴로 돌아 가려면 'b' 입력) ]\n", strlen("[ 비밀번호를 입력하세요 (메뉴로 돌아 가려면 'b' 입력) ]\n"));
+				memset(pw_str, 0, sizeof(pw_str));
 				str_len = read(clnt_sock, pw_str, sizeof(pw_str));
 				printf("입력한 비밀번호 %s", pw_str);
 				pw_str[str_len-1] = '\0';
@@ -163,6 +169,9 @@ void * handle_clnt(void * arg)
 				{
 					if (strcmp(id_str, reg_user_list[i].id) == 0 && strcmp(pw_str, reg_user_list[i].pw) == 0)
 					{
+						p_user = reg_user_list[i]; // 로그인 정보와 일치한 회원을 현재 회원 구조체(p_user)에 복사
+						p_user.socket_num = clnt_sock; // 로그인 성공시 소켓 번호 저장
+						p_user.state = 1; // 상태 로그인(1)으로 변경
 						login_chk = 1;
 						break;
 					}
@@ -191,8 +200,10 @@ void * handle_clnt(void * arg)
 			
 			//============================================ 아이디 ============================================================
 			write(clnt_sock, "[ 회원가입 할 아이디를 입력하세요 (영어, 숫자 사용 // 4~15 글자) ]\n", strlen("[ 회원가입 할 아이디를 입력하세요 (영어, 숫자 사용 // 4~15 글자) ]\n"));
-			while ((str_len = read(clnt_sock, id_str, sizeof(id_str))) != 0) // 아이디 입력
+			while (1) // 아이디 입력
 			{
+				memset(id_str, 0, sizeof(id_str));
+				str_len = read(clnt_sock, id_str, sizeof(id_str));
 				printf("입력한 아이디 %s", id_str);
 
 				id_str[str_len - 1] = '\0';
@@ -226,8 +237,10 @@ void * handle_clnt(void * arg)
 
 			//============================================ 비밀 번호 ====================================================================
 			write(clnt_sock, "[ 비밀번호를 입력하세요 (4~15 글자) ]\n", strlen("[ 비밀번호를 입력하세요 (4~15 글자) ]\n"));
-			while ((str_len = read(clnt_sock, pw_str, sizeof(pw_str))) != 0) // 비밀번호 입력
+			while (1) // 비밀번호 입력
 			{
+				memset(pw_str, 0, sizeof(pw_str));
+				str_len = read(clnt_sock, pw_str, sizeof(pw_str));
 				printf("입력한 비밀번호 %s", pw_str);
 
 				pw_str[str_len - 1] = '\0';
@@ -239,6 +252,7 @@ void * handle_clnt(void * arg)
 				}
 				
 				write(clnt_sock, "[ 비밀번호를 다시 한 번 입력하세요 ]\n", strlen("[ 비밀번호를 다시 한 번 입력하세요 ]\n"));
+				memset(pw_re_str, 0, sizeof(pw_re_str));
 				str_len = read(clnt_sock, pw_re_str, sizeof(pw_str));
 				pw_re_str[str_len - 1] = '\0';
 
@@ -254,8 +268,10 @@ void * handle_clnt(void * arg)
 
 			//============================================ 닉네임 ================================================
 			write(clnt_sock, "[ 닉네임을 입력하세요 (4~15 글자) ]\n", strlen("[ 닉네임을 입력하세요 (4~15 글자) ]\n"));
-			while ((str_len = read(clnt_sock, nick_str, sizeof(nick_str))) != 0) // 닉네임 입력
+			while (1) // 닉네임 입력
 			{
+				memset(nick_str, 0, sizeof(nick_str));
+				str_len = read(clnt_sock, nick_str, sizeof(nick_str));
 				printf("입력한 닉네임 %s", nick_str);
 
 				nick_str[str_len - 1] = '\0';
@@ -289,8 +305,10 @@ void * handle_clnt(void * arg)
 
 			//============================================ 휴대폰 번호 ================================================
 			write(clnt_sock, "[ 휴대폰 번호를 입력하세요 ('-' 제외 숫자만)) ]\n", strlen("[ 휴대폰 번호를 입력하세요 ('-' 제외 숫자만)) ]\n"));
-			while ((str_len = read(clnt_sock, phone_num_str, sizeof(phone_num_str))) != 0) // 휴대폰 번호 입력
+			while (1) // 휴대폰 번호 입력
 			{
+				memset(phone_num_str, 0, sizeof(phone_num_str));
+				str_len = read(clnt_sock, phone_num_str, sizeof(phone_num_str));
 				printf("입력한 휴대폰 번호 %s", phone_num_str);
 
 				phone_num_str[str_len - 1] = '\0';
@@ -323,8 +341,10 @@ void * handle_clnt(void * arg)
 
 			//============================================ 이메일 ================================================
 			write(clnt_sock, "[ 이메일 주소를 입력하세요(30자 이하) ]\n", strlen("[ 이메일 주소를 입력하세요(30자 이하) ]\n"));
-			while ((str_len = read(clnt_sock, e_mail_str, sizeof(e_mail_str))) != 0) // 이메일 주소 입력
+			while (1) // 이메일 주소 입력
 			{
+				memset(e_mail_str, 0, sizeof(e_mail_str));
+				str_len = read(clnt_sock, e_mail_str, sizeof(e_mail_str));
 				printf("입력한 이메일 주소 %s", e_mail_str);
 
 				e_mail_str[str_len - 1] = '\0';
@@ -357,8 +377,10 @@ void * handle_clnt(void * arg)
 
 			//============================================ 비밀번호 찾기 질문 ================================================
 			write(clnt_sock, "[ 비밀번호 찾기에 사용되는 질문을 입력하세요(30자 이하) ]\n", strlen("[ 비밀번호 찾기에 사용되는 질문을 입력하세요(30자 이하) ]\n"));
-			while ((str_len = read(clnt_sock, f_question_str, sizeof(f_question_str))) != 0) // 질문 입력
+			while (1) // 질문 입력
 			{
+				memset(f_question_str, 0, sizeof(f_question_str));
+				str_len = read(clnt_sock, f_question_str, sizeof(f_question_str));
 				printf("입력한 질문 %s", f_question_str);
 
 				f_question_str[str_len - 1] = '\0';
@@ -375,9 +397,11 @@ void * handle_clnt(void * arg)
 
 
 			//============================================ 비밀번호 찾기 정답 ================================================
-			write(clnt_sock, "[ 비밀번호 찾기에 사용되는 정답을 입력하세요(20자 이하) ]\n", strlen("[ 비밀번호 찾기에 사용되는 정답을 입력하세요(20자 이하) ]\n"));
-			while ((str_len = read(clnt_sock, f_answer_str, sizeof(f_answer_str))) != 0) // 정답 입력
+			write(clnt_sock, "[ 비밀번호 찾기에 사용되는 정답을 입력하세요(20자 이하) ]\n", strlen("[ 비밀번호 찾기에 사용되는 정답을 입력하세요(20자 이하) ]\n"));			
+			while (1) // 정답 입력
 			{
+				memset(f_answer_str, 0, sizeof(f_answer_str));
+				str_len = read(clnt_sock, f_answer_str, sizeof(f_answer_str));
 				printf("입력한 정답 %s", f_answer_str);
 
 				f_answer_str[str_len - 1] = '\0';
@@ -395,7 +419,6 @@ void * handle_clnt(void * arg)
 
 			pthread_mutex_lock(&mutx);
 			reg_user_list[reg_user_cnt].user_num = reg_user_cnt + 1;
-			reg_user_list[reg_user_cnt].socket_num = clnt_sock;
 			reg_user_list[reg_user_cnt].state = 2; // 유저 상태 로그아웃으로 바꿈 
 			strcpy(reg_user_list[reg_user_cnt].id, id_str);
 			strcpy(reg_user_list[reg_user_cnt].pw, pw_str);
@@ -413,14 +436,122 @@ void * handle_clnt(void * arg)
 		else if (choice_num == 3) // 아이디 찾기
 		{
 			printf("3번 입력\n");
+			while (1)
+			{
+				back_chk = 0;
+				write(clnt_sock, "[ 휴대폰 번호를 입력하세요. (메뉴로 돌아가려면 'b') ]\n", strlen("[ 휴대폰 번호를 입력하세요. (메뉴로 돌아가려면 'b') ]\n"));
+				memset(phone_num_str, 0, sizeof(phone_num_str));
+				str_len = read(clnt_sock, phone_num_str, sizeof(phone_num_str));
+				printf("입력한 전화번호 %s", phone_num_str);
+				phone_num_str[str_len-1] = '\0';
+
+				if (strcmp(phone_num_str, "b") == 0)
+				{
+					back_chk = 1;
+					break;
+				}
+								
+				pnum_chk = 0;
+				for (i = 0; i < reg_user_cnt; i++)
+				{
+					if (strcmp(phone_num_str, reg_user_list[i].phone_num) == 0)
+					{	
+						pnum_chk = 1;
+						break;
+					}
+					
+				}
+
+				if (pnum_chk == 1)
+				{
+					memset(msg, 0, sizeof(msg));
+					sprintf(msg, "회원님의 ID는 '%s' 입니다. \n", reg_user_list[i].id);
+					write(clnt_sock, msg, strlen(msg));
+					break;
+				}
+				else
+				{
+					write(clnt_sock, "해당 휴대폰 번호로 등록된 ID가 없습니다.\n", strlen("해당 휴대폰 번호로 등록된 ID가 없습니다.\n"));
+					continue;
+				}
+			}
+			if (back_chk == 1)
+				continue;
+			if (pnum_chk == 1)
+				continue;
 		}
 		else if (choice_num == 4) // 비밀번호 찾기
 		{
+			int id_chk, pw_chk;
 			printf("4번 입력\n");
+			while (1)
+			{
+				int index_num;
+				back_chk = 0;
+				write(clnt_sock, "[ 비밀번호를 찾을 ID를 입력하세요. (메뉴로 돌아가려면 'b') ]\n", strlen("[ 비밀번호를 찾을 ID를 입력하세요. (메뉴로 돌아가려면 'b') ]\n"));
+				memset(id_str, 0, sizeof(id_str));
+				str_len = read(clnt_sock, id_str, sizeof(id_str));
+				printf("입력한 ID %s", id_str);
+				id_str[str_len-1] = '\0';
+
+				if (strcmp(id_str, "b") == 0)
+				{
+					back_chk = 1;
+					break;
+				}
+
+				id_chk = 0;
+				pw_chk = 0;
+				for (i = 0; i < reg_user_cnt; i++)
+				{
+					if (strcmp(id_str, reg_user_list[i].id) == 0)
+					{	
+						index_num = i;
+						id_chk = 1;
+						break;
+					}
+					
+				}
+				if (id_chk == 1)
+				{
+					memset(msg, 0, sizeof(msg));
+					sprintf(msg, "질문 : %s\n", reg_user_list[index_num].f_question);
+					write(clnt_sock, msg, strlen(msg));
+					write(clnt_sock, "질문에 대한 정답을 입력하세요.\n", strlen("질문에 대한 정답을 입력하세요.\n"));
+					memset(f_answer_str, 0, sizeof(f_answer_str));
+					str_len = read(clnt_sock, f_answer_str, sizeof(f_answer_str));
+					f_answer_str[str_len-1] = '\0';
+
+					if (strcmp(f_answer_str, reg_user_list[index_num].f_answer) == 0)
+					{
+						memset(msg, 0, sizeof(msg));
+						sprintf(msg, "회원님의 비밀번호는 '%s' 입니다.\n", reg_user_list[index_num].pw);
+						write(clnt_sock, msg, strlen(msg));
+						pw_chk = 1;
+						break;
+					}
+					else
+					{
+						write(clnt_sock, "정답이 일치하지 않습니다.\n", strlen("정답이 일치하지 않습니다.\n"));
+						continue;
+					}
+					break;
+				}
+				else
+				{
+					write(clnt_sock, "등록된 ID가 없습니다.\n", strlen("등록된 ID가 없습니다.\n"));
+					continue;
+				}
+			}
+			if (back_chk == 1)
+				continue;
+			if (pw_chk == 1)
+				continue;
+
 		}
 		else
 		{
-			write(clnt_sock, "번호를 다시 입력하세요!\n", strlen("번호를 다시 입력하세요!\n"));
+			write(clnt_sock, "다시 선택하세요!\n", strlen("다시 선택하세요!\n"));
 		}
 
 	}
@@ -430,8 +561,11 @@ void * handle_clnt(void * arg)
 		printf("%s, %s, %s, %s, %s, %s\n", reg_user_list[i].id, reg_user_list[i].pw, reg_user_list[i].nick_name, reg_user_list[i].phone_num, reg_user_list[i].e_mail, reg_user_list[i].f_question);
 	}
 	
-	while((str_len=read(clnt_sock, msg, sizeof(msg)))!=0)
+	while((str_len = read(clnt_sock, msg, sizeof(msg))) != 0)
 	{
+
+		
+		// memset(msg, 0, sizeof(msg));
 		printf("%s", msg);
 		send_msg(msg, str_len);
 	}
--- project3_ChatProgram/chat_serv_backup.c
+++ project3_ChatProgram/chat_serv_backup.c
@@ -31,15 +31,17 @@ typedef struct user
 	char phone_num[12];
 	char e_mail[40];
 	char f_question[100];
-	char f_answer[20];
+	char f_answer[100];
 	int socket_num;
-	int state;
+	int state; // 1: 로그인, 2: 로그아웃, 3: 회원탈퇴, 
+	int room_num; // 현재 유저의 채팅방 번호 => 로비 채팅방 1번
 	LETTER letter_box[10];
 
 }USER;
 
-USER reg_user_list[MAX_REG_USER] = {0,};
-
+USER reg_user_list[MAX_REG_USER] = 
+{{1, "qwer", "1111", "cookie", "01055555555", "ppp@naver.com", "안녕", "하세요", 0, 2, 0},
+ {2, "ssss1234", "1234", "뚜비", "01044444444", "aoui@naver.com", "반갑", "습니다", 0, 2, 0}};
 
 
 
@@ -47,7 +49,7 @@ void * handle_clnt(void * arg);
 void send_msg(char * msg, int len);
 void error_handling(char * msg);
 
-int reg_user_cnt = 0;
+int reg_user_cnt = 2;
 int clnt_cnt = 0;
 int clnt_socks[MAX_CLNT];
 pthread_mutex_t mutx;
@@ -95,6 +97,7 @@ int main(int argc, char *argv[])
 	
 void * handle_clnt(void * arg)
 {
+	USER p_user = {0,};
 	// int user_num;
 	// char id[MAX_ID_LEN];
 	// char pw[MAX_PW_LEN];
@@ -104,7 +107,8 @@ void * handle_clnt(void * arg)
 	// char f_question[100];
 	// char f_answer[20];
 	int clnt_sock=*((int*)arg);
-	int str_len=0, i, choice_num, dup_chk;
+	int str_len = 0; 
+	int i, choice_num, dup_chk, login_chk, back_chk, pnum_chk;
 	char msg[BUF_SIZE];
 	char choice_str[BUF_SIZE];
 	char id_str[BUF_SIZE];
@@ -116,13 +120,14 @@ void * handle_clnt(void * arg)
 	char f_question_str[BUF_SIZE];
 	char f_answer_str[BUF_SIZE];
 
-	char start_str[] = "짭레그램에 오신걸 환영합니다\n 1.로그인 / 2.회원가입 / 3.ID 찾기 / 4.비밀번호 찾기\n";
+	char start_str[] = "짭레그램에 오신걸 환영합니다\n";
 
 	write(clnt_sock, start_str, strlen(start_str));
 
-	while((str_len = read(clnt_sock, choice_str, sizeof(choice_str))) != 0)
+	while(1)
 	{
-		
+		write(clnt_sock, "1.로그인 / 2.회원가입 / 3.ID 찾기 / 4.비밀번호 찾기\n", strlen("1.로그인 / 2.회원가입 / 3.ID 찾기 / 4.비밀번호 찾기\n"));
+		str_len = read(clnt_sock, choice_str, sizeof(choice_str));
 		printf("입력한 숫자 %s", choice_str);
 		choice_str[str_len-1] = '\0';
 		choice_num = atoi(choice_str);
@@ -131,7 +136,63 @@ void * handle_clnt(void * arg)
 		{
 			printf("1번 입력\n");
 
+			while (1)
+			{
+				back_chk = 0;
 
+				write(clnt_sock, "[ 아이디를 입력하세요 (메뉴로 돌아 가려면 'b' 입력) ]\n", strlen("[ 아이디를 입력하세요 (메뉴로 돌아 가려면 'b' 입력) ]\n"));
+				memset(id_str, 0, sizeof(id_str));
+				str_len = read(clnt_sock, id_str, sizeof(id_str));
+				printf("입력한 아이디 %s", id_str);
+				id_str[str_len-1] = '\0';
+								
+				if (strcmp(id_str, "b") == 0)
+				{
+					back_chk = 1;
+					break;
+				}
+
+				write(clnt_sock, "[ 비밀번호를 입력하세요 (메뉴로 돌아 가려면 'b' 입력) ]\n", strlen("[ 비밀번호를 입력하세요 (메뉴로 돌아 가려면 'b' 입력) ]\n"));
+				memset(pw_str, 0, sizeof(pw_str));
+				str_len = read(clnt_sock, pw_str, sizeof(pw_str));
+				printf("입력한 비밀번호 %s", pw_str);
+				pw_str[str_len-1] = '\0';
+
+				if (strcmp(pw_str, "b") == 0)
+				{
+					back_chk = 1;
+					break;
+				}
+
+				login_chk = 0;
+				for (i = 0; i < reg_user_cnt; i++)
+				{
+					if (strcmp(id_str, reg_user_list[i].id) == 0 && strcmp(pw_str, reg_user_list[i].pw) == 0)
+					{
+						p_user = reg_user_list[i]; // 로그인 정보와 일치한 회원을 현재 회원 구조체(p_user)에 복사
+						p_user.socket_num = clnt_sock; // 로그인 성공시 소켓 번호 저장
+						p_user.state = 1; // 상태 로그인(1)으로 변경
+						login_chk = 1;
+						break;
+					}
+				}
+
+				if (login_chk == 1)
+				{
+					write(clnt_sock, "로그인 성공\n", strlen("로그인 성공\n"));
+					break;
+				}
+				else
+				{
+					write(clnt_sock, "아이디 또는 비밀번호가 맞지 않습니다.\n", strlen("아이디 또는 비밀번호가 맞지 않습니다.\n"));
+					continue;
+				}
+				
+			}
+			if (login_chk == 1)
+				break;
+			if (back_chk == 1)
+				continue;
 		}
 		else if (choice_num == 2) // 회원가입
 		{
@@ -139,8 +200,10 @@ void * handle_clnt(void * arg)
 			
 			//============================================ 아이디 ============================================================
 			write(clnt_sock, "[ 회원가입 할 아이디를 입력하세요 (영어, 숫자 사용 // 4~15 글자) ]\n", strlen("[ 회원가입 할 아이디를 입력하세요 (영어, 숫자 사용 // 4~15 글자) ]\n"));
-			while ((str_len = read(clnt_sock, id_str, sizeof(id_str))) != 0) // 아이디 입력
+			while (1) // 아이디 입력
 			{
+				memset(id_str, 0, sizeof(id_str));
+				str_len = read(clnt_sock, id_str, sizeof(id_str));
 				printf("입력한 아이디 %s", id_str);
 
 				id_str[str_len - 1] = '\0';
@@ -174,8 +237,10 @@ void * handle_clnt(void * arg)
 
 			//============================================ 비밀 번호 ====================================================================
 			write(clnt_sock, "[ 비밀번호를 입력하세요 (4~15 글자) ]\n", strlen("[ 비밀번호를 입력하세요 (4~15 글자) ]\n"));
-			while ((str_len = read(clnt_sock, pw_str, sizeof(pw_str))) != 0) // 비밀번호 입력
+			while (1) // 비밀번호 입력
 			{
+				memset(pw_str, 0, sizeof(pw_str));
+				str_len = read(clnt_sock, pw_str, sizeof(pw_str));
 				printf("입력한 비밀번호 %s", pw_str);
 
 				pw_str[str_len - 1] = '\0';
@@ -187,6 +252,7 @@ void * handle_clnt(void * arg)
 				}
 				
 				write(clnt_sock, "[ 비밀번호를 다시 한 번 입력하세요 ]\n", strlen("[ 비밀번호를 다시 한 번 입력하세요 ]\n"));
+				memset(pw_re_str, 0, sizeof(pw_re_str));
 				str_len = read(clnt_sock, pw_re_str, sizeof(pw_str));
 				pw_re_str[str_len - 1] = '\0';
 
@@ -202,8 +268,10 @@ void * handle_clnt(void * arg)
 
 			//============================================ 닉네임 ================================================
 			write(clnt_sock, "[ 닉네임을 입력하세요 (4~15 글자) ]\n", strlen("[ 닉네임을 입력하세요 (4~15 글자) ]\n"));
-			while ((str_len = read(clnt_sock, nick_str, sizeof(nick_str))) != 0) // 닉네임 입력
+			while (1) // 닉네임 입력
 			{
+				memset(nick_str, 0, sizeof(nick_str));
+				str_len = read(clnt_sock, nick_str, sizeof(nick_str));
 				printf("입력한 닉네임 %s", nick_str);
 
 				nick_str[str_len - 1] = '\0';
@@ -237,8 +305,10 @@ void * handle_clnt(void * arg)
 
 			//============================================ 휴대폰 번호 ================================================
 			write(clnt_sock, "[ 휴대폰 번호를 입력하세요 ('-' 제외 숫자만)) ]\n", strlen("[ 휴대폰 번호를 입력하세요 ('-' 제외 숫자만)) ]\n"));
-			while ((str_len = read(clnt_sock, phone_num_str, sizeof(phone_num_str))) != 0) // 휴대폰 번호 입력
+			while (1) // 휴대폰 번호 입력
 			{
+				memset(phone_num_str, 0, sizeof(phone_num_str));
+				str_len = read(clnt_sock, phone_num_str, sizeof(phone_num_str));
 				printf("입력한 휴대폰 번호 %s", phone_num_str);
 
 				phone_num_str[str_len - 1] = '\0';
@@ -270,24 +340,26 @@ void * handle_clnt(void * arg)
 			printf("검수 완료 휴대폰 번호 %s\n", phone_num_str);
 
 			//============================================ 이메일 ================================================
-			write(clnt_sock, "[ 이메일 주소를 입력하세요 ]\n", strlen("[ 이메일 주소를 입력하세요 ]\n"));
-			while ((str_len = read(clnt_sock, e_mail_str, sizeof(e_mail_str))) != 0) // 이메일 주소 입력
+			write(clnt_sock, "[ 이메일 주소를 입력하세요(30자 이하) ]\n", strlen("[ 이메일 주소를 입력하세요(30자 이하) ]\n"));
+			while (1) // 이메일 주소 입력
 			{
+				memset(e_mail_str, 0, sizeof(e_mail_str));
+				str_len = read(clnt_sock, e_mail_str, sizeof(e_mail_str));
 				printf("입력한 이메일 주소 %s", e_mail_str);
 
 				e_mail_str[str_len - 1] = '\0';
 
-				if (strlen(e_mail_str) != 11) // 대폰 번호 숫자 수 체크
+				if (!(strlen(e_mail_str) >= 1 && strlen(e_mail_str) <= 30)) // 이메일 주소 숫자 수 체크
 				{
-					write(clnt_sock, "휴대폰 번호 숫자 11개를 다시 입력하세요.\n", strlen("휴대폰 번호 숫자 11개를 다시 입력하세요.\n"));
+					write(clnt_sock, "이메일 주소를 다시 입력하세요.\n", strlen("이메일 주소를 다시 입력하세요.\n"));
 					continue;
 				}
 				
 				dup_chk = 0;
 				pthread_mutex_lock(&mutx);
-				for (i = 0; i < MAX_REG_USER; i++) // 휴대폰 번호 중복 체크
+				for (i = 0; i < MAX_REG_USER; i++) // 이메일 주소 중복 체크
 				{
-					if (strcmp(reg_user_list[i].phone_num, phone_num_str) == 0)
+					if (strcmp(reg_user_list[i].e_mail, e_mail_str) == 0)
 					{
 						dup_chk = 1;
 						break;
@@ -296,35 +368,204 @@ void * handle_clnt(void * arg)
 				pthread_mutex_unlock(&mutx);
 				if (dup_chk == 1)
 				{
-					write(clnt_sock, "이미 가입 된 휴대폰 번호 입니다. 다시 입력하세요.\n", strlen("이미 가입 된 휴대폰 번호 입니다. 다시 입력하세요.\n"));
+					write(clnt_sock, "이미 가입 된 이메일 주소 입니다. 다시 입력하세요.\n", strlen("이미 가입 된 이메일 주소 입니다. 다시 입력하세요.\n"));
 					continue;
 				}
 				break;
 			}
-			printf("검수 완료 휴대폰 번호 %s\n", phone_num_str);
-			
+			printf("검수 완료 이메일 주소 %s\n", e_mail_str);
 
+			//============================================ 비밀번호 찾기 질문 ================================================
+			write(clnt_sock, "[ 비밀번호 찾기에 사용되는 질문을 입력하세요(30자 이하) ]\n", strlen("[ 비밀번호 찾기에 사용되는 질문을 입력하세요(30자 이하) ]\n"));
+			while (1) // 질문 입력
+			{
+				memset(f_question_str, 0, sizeof(f_question_str));
+				str_len = read(clnt_sock, f_question_str, sizeof(f_question_str));
+				printf("입력한 질문 %s", f_question_str);
+
+				f_question_str[str_len - 1] = '\0';
+
+				if (!(strlen(f_question_str) >= 1 && strlen(f_question_str) <= 90)) // 질문 숫자 수 체크
+				{
+					write(clnt_sock, "질문을 다시 입력하세요.\n", strlen("질문을 다시 입력하세요.\n"));
+					continue;
+				}
+				
+				break;
+			}
+			printf("검수 완료 질문 %s\n", f_question_str);
+
+
+			//============================================ 비밀번호 찾기 정답 ================================================
+			write(clnt_sock, "[ 비밀번호 찾기에 사용되는 정답을 입력하세요(20자 이하) ]\n", strlen("[ 비밀번호 찾기에 사용되는 정답을 입력하세요(20자 이하) ]\n"));			
+			while (1) // 정답 입력
+			{
+				memset(f_answer_str, 0, sizeof(f_answer_str));
+				str_len = read(clnt_sock, f_answer_str, sizeof(f_answer_str));
+				printf("입력한 정답 %s", f_answer_str);
+
+				f_answer_str[str_len - 1] = '\0';
+
+				if (!(strlen(f_answer_str) >= 1 && strlen(f_answer_str) <= 60)) // 정답 숫자 수 체크
+				{
+					write(clnt_sock, "정답을 다시 입력하세요.\n", strlen("정답을 다시 입력하세요.\n"));
+					continue;
+				}
+				
+				break;
+			}
+			printf("검수 완료 질문 %s\n", f_answer_str);
+
+
+			pthread_mutex_lock(&mutx);
+			reg_user_list[reg_user_cnt].user_num = reg_user_cnt + 1;
+			reg_user_list[reg_user_cnt].state = 2; // 유저 상태 로그아웃으로 바꿈 
+			strcpy(reg_user_list[reg_user_cnt].id, id_str);
+			strcpy(reg_user_list[reg_user_cnt].pw, pw_str);
+			strcpy(reg_user_list[reg_user_cnt].nick_name, nick_str);
+			strcpy(reg_user_list[reg_user_cnt].phone_num, phone_num_str);
+			strcpy(reg_user_list[reg_user_cnt].e_mail, e_mail_str);
+			strcpy(reg_user_list[reg_user_cnt].f_question, f_question_str);
+			strcpy(reg_user_list[reg_user_cnt].f_answer, f_answer_str);
+			reg_user_cnt++;
+			pthread_mutex_unlock(&mutx);
+
+			write(clnt_sock, "회원 가입이 완료 되었습니다! 로그인 하세요.\n\n", strlen("회원 가입이 완료 되었습니다! 로그인 하세요.\n\n"));
+			continue;
 		}
 		else if (choice_num == 3) // 아이디 찾기
 		{
 			printf("3번 입력\n");
+			while (1)
+			{
+				back_chk = 0;
+				write(clnt_sock, "[ 휴대폰 번호를 입력하세요. (메뉴로 돌아가려면 'b') ]\n", strlen("[ 휴대폰 번호를 입력하세요. (메뉴로 돌아가려면 'b') ]\n"));
+				memset(phone_num_str, 0, sizeof(phone_num_str));
+				str_len = read(clnt_sock, phone_num_str, sizeof(phone_num_str));
+				printf("입력한 전화번호 %s", phone_num_str);
+				phone_num_str[str_len-1] = '\0';
+
+				if (strcmp(phone_num_str, "b") == 0)
+				{
+					back_chk = 1;
+					break;
+				}
+								
+				pnum_chk = 0;
+				for (i = 0; i < reg_user_cnt; i++)
+				{
+					if (strcmp(phone_num_str, reg_user_list[i].phone_num) == 0)
+					{	
+						pnum_chk = 1;
+						break;
+					}
+					
+				}
+
+				if (pnum_chk == 1)
+				{
+					memset(msg, 0, sizeof(msg));
+					sprintf(msg, "회원님의 ID는 '%s' 입니다. \n", reg_user_list[i].id);
+					write(clnt_sock, msg, strlen(msg));
+					break;
+				}
+				else
+				{
+					write(clnt_sock, "해당 휴대폰 번호로 등록된 ID가 없습니다.\n", strlen("해당 휴대폰 번호로 등록된 ID가 없습니다.\n"));
+					continue;
+				}
+			}
+			if (back_chk == 1)
+				continue;
+			if (pnum_chk == 1)
+				continue;
 		}
 		else if (choice_num == 4) // 비밀번호 찾기
 		{
+			int id_chk, pw_chk;
 			printf("4번 입력\n");
+			while (1)
+			{
+				int index_num;
+				back_chk = 0;
+				write(clnt_sock, "[ 비밀번호를 찾을 ID를 입력하세요. (메뉴로 돌아가려면 'b') ]\n", strlen("[ 비밀번호를 찾을 ID를 입력하세요. (메뉴로 돌아가려면 'b') ]\n"));
+				memset(id_str, 0, sizeof(id_str));
+				str_len = read(clnt_sock, id_str, sizeof(id_str));
+				printf("입력한 ID %s", id_str);
+				id_str[str_len-1] = '\0';
+
+				if (strcmp(id_str, "b") == 0)
+				{
+					back_chk = 1;
+					break;
+				}
+
+				id_chk = 0;
+				pw_chk = 0;
+				for (i = 0; i < reg_user_cnt; i++)
+				{
+					if (strcmp(id_str, reg_user_list[i].id) == 0)
+					{	
+						index_num = i;
+						id_chk = 1;
+						break;
+					}
+					
+				}
+				if (id_chk == 1)
+				{
+					memset(msg, 0, sizeof(msg));
+					sprintf(msg, "질문 : %s\n", reg_user_list[index_num].f_question);
+					write(clnt_sock, msg, strlen(msg));
+					write(clnt_sock, "질문에 대한 정답을 입력하세요.\n", strlen("질문에 대한 정답을 입력하세요.\n"));
+					memset(f_answer_str, 0, sizeof(f_answer_str));
+					str_len = read(clnt_sock, f_answer_str, sizeof(f_answer_str));
+					f_answer_str[str_len-1] = '\0';
+
+					if (strcmp(f_answer_str, reg_user_list[index_num].f_answer) == 0)
+					{
+						memset(msg, 0, sizeof(msg));
+						sprintf(msg, "회원님의 비밀번호는 '%s' 입니다.\n", reg_user_list[index_num].pw);
+						write(clnt_sock, msg, strlen(msg));
+						pw_chk = 1;
+						break;
+					}
+					else
+					{
+						write(clnt_sock, "정답이 일치하지 않습니다.\n", strlen("정답이 일치하지 않습니다.\n"));
+						continue;
+					}
+					break;
+				}
+				else
+				{
+					write(clnt_sock, "등록된 ID가 없습니다.\n", strlen("등록된 ID가 없습니다.\n"));
+					continue;
+				}
+			}
+			if (back_chk == 1)
+				continue;
+			if (pw_chk == 1)
+				continue;
+
 		}
 		else
 		{
-			write(clnt_sock, "번호를 다시 입력하세요!\n", strlen("번호를 다시 입력하세요!\n"));
+			write(clnt_sock, "다시 선택하세요!\n", strlen("다시 선택하세요!\n"));
 		}
-
 
 	}
 	
-	
-	
-	while((str_len=read(clnt_sock, msg, sizeof(msg)))!=0)
+	for (i = 0; i < reg_user_cnt; i++)
 	{
+		printf("%s, %s, %s, %s, %s, %s\n", reg_user_list[i].id, reg_user_list[i].pw, reg_user_list[i].nick_name, reg_user_list[i].phone_num, reg_user_list[i].e_mail, reg_user_list[i].f_question);
+	}
+	
+	while((str_len = read(clnt_sock, msg, sizeof(msg))) != 0)
+	{
+
+		
+		// memset(msg, 0, sizeof(msg));
 		printf("%s", msg);
 		send_msg(msg, str_len);
 	}
