f6b30e7ba3345e60084997fca1c7c68259cf9863
myeonghh <poiuson123@naver.com>
2024-10-01 18:31:13 +0900

    채팅 프초그램

--- project4_Card/card_serv3.cpp
+++ project4_Card/card_serv3.cpp
@@ -7,8 +7,10 @@
 #include <netinet/in.h>
 #include <unistd.h>
 #include <mutex>
+#include <vector>
+#include <string>
 
-#define BUF_SIZE 100
+#define BUF_SIZE 1000
 #define MAX_CLNT 256
 
 std::list<int> clnt_socks;  // 소켓 목록을 관리할 리스트
@@ -18,35 +20,203 @@ void handle_clnt(int clnt_sock);
 void send_msg(const char * msg, int len);
 void error_handling(const char * msg);
 
+enum {Sinhan = 1, KB, NH, Samsung, Woori, Hana};
+
+
 class User
 {
 private:
     int user_num;
     std::string id;
+    std::string pw;
     std::string name;
     int age;
     bool sex;
 public:
-    User(int u_num, std::string id, std::string name, int age, bool sex) : user_num(u_num), id(id), name(name), age(age), sex(sex)
+    User() : user_num(0), id(NULL), pw(NULL), name(NULL), age(0), sex(NULL) 
     {}
     int get_user_num() const {return user_num;}
     std::string get_id() const {return id;}
+    std::string get_pw() const {return pw;}
     std::string get_name() const {return name;}
     int get_age() const {return age;}
     int get_sex() const {return sex;}
+};
+
+class UserHandler
+{
+private:
+    std::vector<User*> user_v;
+    int clnt_sock;
+    
+public:
+    UserHandler(int clnt_sock) : clnt_sock(clnt_sock)
+    {}
+
+    int login(void)
+    {
+        char id_str[BUF_SIZE];
+        char pw_str[BUF_SIZE];
+        int str_len, back_chk;
+
+        while (true)
+        {
+            back_chk = 0;
+
+            write(clnt_sock, "[ 아이디를 입력하세요 (메뉴로 돌아 가려면 'b' 입력) ]\n", strlen("[ 아이디를 입력하세요 (메뉴로 돌아 가려면 'b' 입력) ]\n"));
+            memset(id_str, 0, sizeof(id_str));
+            str_len = read(clnt_sock, id_str, sizeof(id_str));
+            std::cout<<"입력한 아이디 "<<id_str<<std::endl;
+            
+            id_str[str_len-1] = '\0';		
+            if (strcmp(id_str, "b") == 0)
+            {
+                back_chk = 1;
+                return 0;
+            }
+            write(clnt_sock, "[ 비밀번호를 입력하세요 (메뉴로 돌아 가려면 'b' 입력) ]\n", strlen("[ 비밀번호를 입력하세요 (메뉴로 돌아 가려면 'b' 입력) ]\n"));
+            memset(pw_str, 0, sizeof(pw_str));
+            str_len = read(clnt_sock, pw_str, sizeof(pw_str));
+            printf("입력한 비밀번호 %s", pw_str);
+            pw_str[str_len-1] = '\0';
+            if (strcmp(pw_str, "b") == 0)
+            {
+                back_chk = 1;
+                return 0;
+            }
+            for (User* v : user_v)
+            {
+                if (strcmp(v->get_id().c_str(), id_str) == 0 && strcmp(v->get_pw().c_str(), pw_str) == 0) // 로그인 성공
+                {
+
+                }
+            }
+            
+                // p_user = reg_user_list[i];
+                // p_user.state = 1;
+                // p_user.socket_num = clnt_sock; // 로그인 성공시 자신의 소켓 번호 저장
+                // p_user.room_num = 1; // 현재 들어있는 방을 1로 바꿈(로비 채팅방)
+                // login_user_list[login_user_cnt] = p_user; // 로그인 유저 리스트에 로그인 성공 유저 구조체 담기
+                // login_user_cnt++; // 로그인 유저 리스트의 인덱스 값을 1 증가
+                // room_list[0].user_cnt += 1;
+                // login_chk = 1;
+                // break;
+        
+        
+            // if (login_chk == 1)
+            // {
+            //     break;
+            // }
+            // else if (login_chk == 2)
+            // {
+            //     write(clnt_sock, "\033[38;2;231;41;41m이미 접속중인 아이디 입니다.\033[0m\n", strlen("\033[38;2;231;41;41m이미 접속중인 아이디 입니다.\033[0m\n"));
+            //     continue;
+            // }
+            // else if (login_chk == 3)
+            // {
+            //     write(clnt_sock, "\033[38;2;231;41;41m해당 아이디는 회원 탈퇴된 계정입니다.\033[0m\n", strlen("\033[38;2;231;41;41m해당 아이디는 회원 탈퇴된 계정입니다.\033[0m\n"));
+            //     continue;
+            // }
+            // else
+            // {
+            //     write(clnt_sock, "\033[38;2;231;41;41m아이디 또는 비밀번호가 맞지 않습니다.\033[0m\n", strlen("\033[38;2;231;41;41m아이디 또는 비밀번호가 맞지 않습니다.\033[0m\n"));
+            //     continue;
+            // }
+        }
+
+    }
+
+    void sign_up(void)
+    {
+
+    }
+    
+    ~UserHandler()
+    {
+
+    };
+
 };
 
 
 class Card
 {
 private:
-    int card_id;
-    std::string card_name;
-    
-    
+    int card_id; // 카드 일련번호
+    std::string card_name; // 카드 이름
+    int anufee; // 카드 연회비
+    bool credit_chk; // 신용, 체크카드 여부
+    std::string brand_name;
+    int brand;
+public:
+    Card(int c_id, std::string c_name, int anufee, bool credit_chk, std::string b_name, int brand) : card_id(c_id), card_name(c_name), anufee(anufee), credit_chk(credit_chk), brand_name(b_name), brand(brand)
+    {}
+    int get_cid() const {return card_id;}
+    std::string get_cname() const {return card_name;}
+    int get_anufee() const {return anufee;}
+    bool get_cchk() const {return credit_chk;}
+    std::string get_bname() const {return brand_name;}
+    int get_brand() const {return brand;}
+};
+
+class SinhanCard : Card
+{
+private:
+public:
+    SinhanCard(int c_id, std::string c_name, int anufee, bool credit_chk, std::string b_name, int brand)
+        : Card(c_id, c_name, anufee, credit_chk, b_name, brand)
+    {}
 
 };
 
+class KBCard : Card
+{
+private:
+public:
+    KBCard(int c_id, std::string c_name, int anufee, bool credit_chk, std::string b_name, int brand)
+        : Card(c_id, c_name, anufee, credit_chk, b_name, brand)
+    {}
+
+};
+
+class NHCard : Card
+{
+private:
+public:
+    NHCard(int c_id, std::string c_name, int anufee, bool credit_chk, std::string b_name, int brand)
+        : Card(c_id, c_name, anufee, credit_chk, b_name, brand)
+    {}
+};
+
+class SamsungCard : Card
+{
+private:
+public:
+    SamsungCard(int c_id, std::string c_name, int anufee, bool credit_chk, std::string b_name, int brand)
+        : Card(c_id, c_name, anufee, credit_chk, b_name, brand)
+    {}
+
+};
+
+class WooriCard : Card
+{
+private:
+public:
+    WooriCard(int c_id, std::string c_name, int anufee, bool credit_chk, std::string b_name, int brand)
+        : Card(c_id, c_name, anufee, credit_chk, b_name, brand)
+    {}
+
+};
+
+class HanaCard : Card
+{
+private:
+public:
+    HanaCard(int c_id, std::string c_name, int anufee, bool credit_chk, std::string b_name, int brand)
+        : Card(c_id, c_name, anufee, credit_chk, b_name, brand)
+    {}
+
+};
 
 
 int main(int argc, char *argv[])
@@ -100,9 +270,32 @@ int main(int argc, char *argv[])
 void handle_clnt(int clnt_sock)
 {
     char msg[BUF_SIZE];
-    int str_len;
+    int str_len, choice, check;
+    UserHandler u_handelr(clnt_sock);
 
-    while (true) 
+    write(clnt_sock, "카추추에 오신걸 환영합니다\n\n", strlen("카추추에 오신걸 환영합니다\n\n"));
+    while (true)
+    {
+        write(clnt_sock, "1. 로그인  2. 회원가입\n", strlen("1. 로그인  2. 회원가입\n"));
+        memset(msg, 0, sizeof(msg));
+        str_len = read(clnt_sock, msg, sizeof(msg));
+		msg[str_len-1] = '\0';
+		choice = atoi(msg);
+
+        check = 0;
+        if (choice == 1)
+        {
+            check = u_handelr.login();
+        }
+        else if (choice == 2)
+        {
+            u_handelr.sign_up();
+        }
+
+    }
+    
+    
+    while (true) // 해당 클라이언트가 접속종료했을때
     {
         str_len = read(clnt_sock, msg, sizeof(msg));
         if (str_len == 0)
---
+++ project4_Card/dbconn.cpp
@@ -0,0 +1,152 @@
+#include <iostream>
+#include <cstring>
+#include <mariadb/conncpp.hpp>
+
+// Delete a task record (indicated by id)
+// 미리 문장을 준비시키고 (prepareStatement),?자리에 변수를 대입시킨다.
+// executeQuery() 마리아디비 로 쿼리를 문자열로 보내실행 시킨다. 
+void deleteTask(std::unique_ptr<sql::Connection> &conn, int id) {
+    try {
+        // Create a new PreparedStatement
+        std::unique_ptr<sql::PreparedStatement> stmnt(conn->prepareStatement("delete from User where id = ?"));
+        // Bind values to SQL statement
+        stmnt->setInt(1, id);
+        // Execute query
+        stmnt->executeQuery();
+    }
+    catch(sql::SQLException& e){
+        std::cerr << "Error deleting task: " << e.what() << std::endl;
+    }
+}
+
+// Update the completed value of a task record (indicated by id)
+void updateTaskStatus(std::unique_ptr<sql::Connection> &conn, std::string id, std::string pw) {
+    try {
+        // Create a new PreparedStatement
+        // ? 자리의 값은 매개변수로 받아옴 
+        // setString(1, pw) : 첫번째 ?자리에 문자열 pw값을 넣겠다.
+        
+        std::unique_ptr<sql::PreparedStatement> stmnt(conn->prepareStatement("update User set pw = ? where id = ?"));
+        // Bind values to SQL statement
+        stmnt->setString(1, pw);
+        stmnt->setString(2, id);
+        // Execute query
+        stmnt->executeQuery();
+    }
+    catch(sql::SQLException& e){
+        std::cerr << "Error updating task status: " << e.what() << std::endl;
+    }
+}
+
+// Create a new task record
+void addTask(std::unique_ptr<sql::Connection> &conn, std::string id_insert, std::string pw_insert, std::string ph_insert) {
+    try {
+        std::cout << id_insert,pw_insert,ph_insert; 
+        // Create a new PreparedStatement
+        std::unique_ptr<sql::PreparedStatement> stmnt(conn->prepareStatement("insert into User (id, pw, phone) values (?,?,?)"));
+        // Bind values to SQL statement
+        stmnt->setString(1, id_insert);
+        stmnt->setString(2, pw_insert);
+        stmnt->setString(3, ph_insert);
+        // Execute query
+        stmnt->executeQuery();
+    }
+    catch(sql::SQLException& e){
+        std::cerr << "Error inserting new task: " << e.what() << std::endl;
+    }
+}
+
+// Print all records in tasks table
+void showTasks(std::unique_ptr<sql::Connection> &conn) {
+    try {
+        // Create a new Statement
+        std::unique_ptr<sql::Statement> stmnt(conn->createStatement());
+        // Execute query
+        sql::ResultSet *res = stmnt->executeQuery("select id,pw,phone from User");
+        // Loop through and print results
+        //쿼리 실행 끝나고 next()함수를 사용하여 한줄한줄 실행되고 넘어감 
+        // while에 next()가 한줄 끝나고 다음줄의 내용이 없을때까지 값을 읽어온다
+        // select 쿼리문 사용 시 필요 
+        while (res->next()) {
+            std::cout << "id = " << res->getString(1);
+            std::cout << ", pw = " << res->getString(2);
+            std::cout << ", phone = " << res->getString(3) << "\n";
+        }
+    }
+    catch(sql::SQLException& e){
+        std::cerr << "Error selecting tasks: " << e.what() << std::endl;
+    }
+}
+
+
+// 반환값을 받아서 쿼리문으로 보내는데 받을때는 어떤형태로 받아오는지 확인해야한다. (sql::ResultSet *res 출력해서 클래스에 넣을 거냐 구조체에 넣을거냐 내가 보기 편하게 만들어야한다.)
+// 출력을 하게 되는데 ./testdb2 showTasks명령어를 치면 select 로 db쿼리를 보내면 값이 출력된다.
+// 터미널 컴파일 명령어 QT는 profile안에서 알아서 찾아서 해라 를 포함시켜놨다.
+// profile안에 라이브러리 찾는 옵션 추가해주심 (.pro가 profile) 추가를 안하면 터미널 명령어가 길어짐
+// mariadb https://mariadb.com/resources/blog/how-to-connect-c-programs-to-mariadb/ 코드 설명 보면 참고 할 수 있다. 번역을 돌려서 봐보자
+
+// Main Process
+int main(int argc, char **argv){
+
+    if (argc==1){
+        std::cout << "Please provide an argument.\n";
+    }
+    else {
+        try {
+            // Instantiate Driver
+            //mariadb 드라이버를 가져와서 시작
+            sql::Driver* driver = sql::mariadb::get_driver_instance();
+
+            // Configure Connection
+            // (localhost부분)ip를 통해 외부에서 접속가능 / 포트는 마리아db접속 포트 / todo는 DB명 
+            // SQLString : url를 통해 
+            // Properties : DB 유저 아이디 와 비밀번호 
+            sql::SQLString url("jdbc:mariadb://localhost:3306/todo");
+            sql::Properties properties({{"user","root"}, { "password","1234"}});
+
+            // Establish Connection
+            // url / properties 를 driver 안에 연결 매개변수 url과 properties를 넘긴다. 
+            // 연결 요청을 보내는곳 (mariadb와)
+            // 
+            std::unique_ptr<sql::Connection> conn(driver->connect(url, properties));
+
+            // Use arguments to determine execution next steps
+            if (!strcmp(argv[1],"showTasks")) {
+                showTasks(conn);
+                return 1;
+            }
+            else if (!strcmp(argv[1],"addTask")) {
+                if (argc != 5) {
+                    std::cout << "Invalid arguments";
+                    return 1;
+                }
+                addTask(conn, argv[2],argv[3],argv[4]);
+            }
+            else if (!strcmp(argv[1],"updateTaskStatus")) {
+                if (argc != 4) {
+                    std::cout << "Invalid arguments";
+                    return 1;
+                }
+                updateTaskStatus(conn, argv[2], argv[3]);
+            }
+            else if (!strcmp(argv[1],"deleteTask")) {
+                if (argc != 3) {
+                    std::cout << "Invalid arguments";
+                    return 1;
+                }
+                deleteTask(conn, atoi(argv[2]));
+            }
+
+            // Close Connection
+            conn->close();
+        }
+        catch(sql::SQLException& e){
+            std::cerr << "Error Connecting to MariaDB Platform: " << e.what() << std::endl;
+            // Exit (Failed)
+            return 1;
+        }
+    }
+
+    // Exit (Success)
+    return 0;
+}
